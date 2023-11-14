357a098b8c2278b146d77bb1b34e8a46c0412de9
Céline Eap <eapceline@gmail.com>
2023-11-14 16:50:43 +0100

    :construction:

--- js/index.js
+++ js/index.js
@@ -1,1 +1,34 @@
-console.log('hello world');
+document.addEventListener('DOMContentLoaded', function() {
+  const dropdown = document.querySelector('.dropdown');
+  const dropdownContent = document.querySelector('.dropdown-content');
+
+  dropdown.for('click', function() {
+    dropdownContent.style.display = dropdownContent.style.display === 'block' ? 'none' : 'block';
+  });
+});
+
+var imgs = document.querySelectorAll('.slider img');
+var dots = document.querySelectorAll('.dot');
+var currentImg = 0; // index of the first image 
+const interval = 3000; // duration(speed) of the slide
+
+function changeSlide(n) {
+  for (var i = 0; i < imgs.length; i++) { // reset
+    imgs[i].style.opacity = 0;
+    dots[i].className = dots[i].className.replace(' active', '');
+  }
+
+  currentImg = n;
+
+  imgs[currentImg].style.opacity = 1;
+  dots[currentImg].className += ' active';
+}
+
+// var timer = setInterval(changeSlide, interval);
+// currentImg = (currentImg + 1) % imgs.length; // update the index number
+
+// if (n != undefined) {
+//     clearInterval(timer);
+//     timer = setInterval(changeSlide, interval);
+//     currentImg = n;
+// }
