a60db6a8d6dca12686255fe72298ef84e97c6326
Arthur Pouteau <arthur.pouteau92@gmail.com>
2023-11-15 17:29:05 +0100

    Auto stash before merge of "main" and "origin/main"

--- js/index.js
+++ js/index.js
@@ -1,19 +1,19 @@
-// document.addEventListener('DOMContentLoaded', function() {
-//   const dropdown = document.querySelector('.dropdown');
-//   const dropdownContent = document.querySelector('.dropdown-content');
 
-//   dropdown.addEventListener('click', function() {
-//     dropdownContent.style.display = dropdownContent.style.display === 'block' ? 'none' : 'block';
-//   });
-// });
-
-var imgs = document.querySelectorAll('.slider img');
-var dots = document.querySelectorAll('.dot');
-var currentImg = 0; // index of the first image 
+let imgs = document.querySelectorAll('.slider img');
+let dots = document.querySelectorAll('.dot');
+let currentImg = 0; // index of the first image 
 const interval = 3000; // duration(speed) of the slide
 
+let selectableItems = document.querySelectorAll('.selectable');
+let tabs = document.querySelectorAll('.tab');
+let tabsContent = document.querySelectorAll('.tab-content');
+
+let selectedItems;
+
+let currentIndex = 0;
+
 function changeSlide(n) {
-  for (var i = 0; i < imgs.length; i++) { // reset
+  for (let i = 0; i < imgs.length; i++) { // reset
     imgs[i].style.opacity = 0;
     dots[i].className = dots[i].className.replace(' active', '');
   }
@@ -23,3 +23,42 @@ function changeSlide(n) {
   imgs[currentImg].style.opacity = 1;
   dots[currentImg].className += ' active';
 }
+
+
+
+function updateSelection() {
+  selectableItems.forEach((item, index) => {
+    if (index === currentIndex) {
+      item.classList.add('active');
+    } else {
+      item.classList.remove('active');
+    }
+  });
+}
+function showTab(dataTabId) {
+  tabsContent.forEach((tabContent) => {
+    if (tabContent.getAttribute('data-tab') === dataTabId) {
+      tabContent.classList.add('tab-content-active');
+    } else {
+      tabContent.classList.remove('tab-content-active');
+    }
+  });
+}
+
+document.addEventListener('keydown', function(event) {
+  switch (event.code) {
+    case 'ArrowRight': // touche pour passer à l'item suivant
+      currentIndex = (currentIndex + 1) % selectableItems.length;
+      break;
+    case 'ArrowLeft': // touche retour
+      currentIndex = (currentIndex - 1 + selectableItems.length) % selectableItems.length;
+      break;
+    case 'Enter': // touche pour valider la selection
+      selectedItems = selectableItems[currentIndex];
+      showTab(selectedItems.getAttribute('data-tab'));
+      break;
+  }
+  updateSelection();
+});
+
+updateSelection();
